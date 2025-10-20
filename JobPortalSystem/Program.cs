88ae9d162e0f2c64487fc216458fc011288697f9
Hossam <hossamghonim97@gmail.com>
2025-10-21 00:50:32 +0300

    hksa

--- JobPortalSystem/Program.cs
+++ JobPortalSystem/Program.cs
@@ -11,6 +11,7 @@ namespace JobPortalSystem
 {
     public class Program
     {
+        
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
