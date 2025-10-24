b87136a0c6591f7b87b7a36b744cc03b0749bbe3
Mohamed Basiouny <mohamedbasiouny111@gmail.com>
2025-10-24 13:51:04 +0300

    Edit Jobs

--- JobPortalSystem/ViewModels/JobsAndCategoriesVM.cs
+++ JobPortalSystem/ViewModels/JobsAndCategoriesVM.cs
@@ -5,7 +5,7 @@ namespace JobPortalSystem.ViewModels
     public class JobsAndCategoriesVM
     {
         public IEnumerable<Job> Jobs { get; set; } = new List<Job>();
-        //public IEnumerable<Job> jobs { get; internal set; }
+        public IEnumerable<Job> jobs { get; internal set; }
         public IEnumerable<JobCategory> Categories { get; set; } = new List<JobCategory>();
         public List<int> appliedJobIds { get; set; } = new List<int>();
     }
